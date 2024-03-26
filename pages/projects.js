6513d89c5b240a2a40b855674267a04a1843fafa
Ismail M <ismail.mohammad204@gmail.com>
2024-03-26 13:34:56 -0500

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -3,8 +3,8 @@ import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 
-import kayakImg from '../public/images/works/kayak.png'
-import courseraImg from '../public/images/works/coursera.png'
+import weatherImg from '../public/images/works/weather.png'
+import secretImg from '../public/images/works/secret.png'
 
 /**
  * Page where users can find information about personal projects I have done. 
@@ -26,12 +26,12 @@ const Works = () => (
 
             <SimpleGrid columns={[1, 1, 3]} gap={6}>
                 <Section delay={0.3}>
-                   <WorkGridItem id="secrets" title="Secret Sharer" thumbnail={kayakImg}>
+                   <WorkGridItem id="secrets" title="Secret Sharer" thumbnail={secretImg}>
                         This project was created as a way to help my friend stop spilling secrets to people outside of our apartment. 
                     </WorkGridItem>
                 </Section>
                 <Section delay={0.3}>
-                    <WorkGridItem id="weatherapp" title="Weather Extension" thumbnail={courseraImg}>
+                    <WorkGridItem id="weatherapp" title="Weather Extension" thumbnail={weatherImg}>
                         Creating an extension that displayed the weather and reccomends clothes I should wear depending on the wear 
                     </WorkGridItem> 
                 </Section>
