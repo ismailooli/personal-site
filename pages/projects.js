561bf55ca8aafb2bf6aba91073b59fa8e2fdc2cf
Ismail M <ismail.mohammad204@gmail.com>
2024-03-26 13:25:50 -0500

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -23,14 +23,14 @@ const Works = () => (
 
             <SimpleGrid columns={[1, 1, 3]} gap={6}>
                 <Section delay={0.3}>
-                    {/* <WorkGridItem id="kayak" title="Kayak" thumbnail={kayakImg}>
-                        This project involved examining the usability of Kayak, a travel booking platform. Travelers of varying expeirence levels were interviewed. 
-                    </WorkGridItem> */}
+                   <WorkGridItem id="secrets" title="Secret Sharer" thumbnail={kayakImg}>
+                        This project was created as a way to help my friend stop spilling secrets to people outside of our apartment. 
+                    </WorkGridItem>
                 </Section>
                 <Section delay={0.3}>
-                    {/* <WorkGridItem id="coursera" title="Coursera" thumbnail={courseraImg}>
-                        An analysis of the level of adherence to usability principles of the Coursera Discussion Forum using heuristic evaluation. 
-                    </WorkGridItem> */}
+                    <WorkGridItem id="weatherapp" title="Weather Extension" thumbnail={courseraImg}>
+                        Creating an extension that displayed the weather and reccomends clothes I should wear depending on the wear 
+                    </WorkGridItem> 
                 </Section>
             </SimpleGrid>
         </Container>
