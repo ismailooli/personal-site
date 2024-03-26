90cb99886dd70fd7e66bbcee302b2bfd1c8b662b
Ismail M <ismail.mohammad204@gmail.com>
2024-03-26 14:01:29 -0500

    Update secret.js to display correct information

--- pages/works/secret.js
+++ pages/works/secret.js
@@ -8,34 +8,30 @@ const Project = () => (
     <Layout>
         <Container>
             <Title>
-                Coursera <Badge>2022</Badge>
+                Secret Sharer <Badge>2023</Badge>
             </Title>
             <WorkImage
-                src="/images/works/coursera.png"
-                alt="Coursera Logo"
+                src="/images/works/secret.png"
+                alt="Secret Sharer Logo"
             />
             <P>
-                This mini-project invovled making use of heuristic evaluation, a technique where the user interface 
-                is evaluated against a list of high-priority usability principles, The list of principles that I used for this 
-                project were created by the <Link href="https://www.nngroup.com/">Nielsen Norman Group</Link>, and can be found <Link href="https://www.nngroup.com/articles/ten-usability-heuristics/">here</Link>. 
-            </P>
+My close friend has trouble navigating social cues when we are in large groups of people. This has lead to times where he would unknowingly spill secrets of others to people who he shouldn’t have. To help him tackle this problem, I decided to create a program that would help he decided to tell someone a secret or not.            </P>
 
 
             <br>
             </br>
 
+                    <P>
+            The frontend was HTML, and CSS. The back end was done with Python and Flask file that used a sentiment analysis model. I used the Hugging Face API to access a basic sentiment analysis model. Depending on what sentiment model feels,  The user inputs a description of the person they want to talk to and the sentiment analysis model 
+                    </P>
             <List>
                 <ListItem>
-                    <Meta>Link</Meta>
-                    <Link target="_blank" href="https://docs.google.com/document/d/1IkyyjPdFBilIQL4dvq7tIBhv6H22HnstM8nFNfcc-pA/edit?usp=sharing">
+                    <Meta>Repo Link</Meta>
+                    <Link target="_blank" href="https://github.com/ismailooli/secret-sharer">
                         Document
                     </Link> <ExternalLinkIcon></ExternalLinkIcon>
                 </ListItem>
-  
-                <ListItem>
-                    <Meta>Methods</Meta>
-                    Heuristic Evaluation, Usability Testing
-                </ListItem>
+   
             </List>
             <br/>
         </Container>
